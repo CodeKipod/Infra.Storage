1fa20b5fff46bdf494880e15048c34c840224a98
Roman Ambinder <roman.ambinder@gmail.com>
2020-11-14 20:35:28 +0200

    Dapper WIP

--- Impl/Dapper/Roman.Ambinder.Storage.Impl.Dapper/DapperSingleKeyRepositoryFor.cs
+++ Impl/Dapper/Roman.Ambinder.Storage.Impl.Dapper/DapperSingleKeyRepositoryFor.cs
@@ -1,8 +1,11 @@
-﻿using Roman.Ambinder.DataTypes.OperationResults;
+﻿using Dapper;
+using Dapper.Contrib.Extensions;
+using ExpressionExtensionSQL;
+using Roman.Ambinder.DataTypes.OperationResults;
 using Roman.Ambinder.Storage.Common.DataTypes;
-using Roman.Ambinder.Storage.Common.Interfaces.SingleKey;
+using Roman.Ambinder.Storage.Common.Interfaces.Common;
 using System;
-using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading;
@@ -11,65 +14,114 @@ using System.Threading.Tasks;
 namespace Roman.Ambinder.Storage.Impl.Dapper
 {
     public class DapperSingleKeyRepositoryFor<TKey, TEntity> :
-        ISingleKeyRepositoryFor<TKey, TEntity>
+        IRepositoryFor<TKey, TEntity>
         where TEntity : class, new()
     {
-        public DapperSingleKeyRepositoryFor()
+        private readonly IDbConnectionProvider _dbConnectionProvider;
+        private readonly string _tableName;
+
+        public DapperSingleKeyRepositoryFor(IDbConnectionProvider dbConnectionProvider,
+            string tableName)
         {
+            _dbConnectionProvider = dbConnectionProvider;
+            _tableName = tableName;
+        }
+
+        private async Task<DbConnection> GetOpenedConnectionAsync()
+        {
+            var connection = _dbConnectionProvider.Get();
+            await connection.OpenAsync().ConfigureAwait(false);
+            
+            return connection;
         }
 
         public Task<OperationResultOf<TEntity>> TryAddAsync(
             Action<TEntity> initAction = null,
             CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            var newEntity = new TEntity();
+            initAction?.Invoke(newEntity);
+
+            return TryAddAsync(newEntity, cancellationToken);
         }
 
-        public Task<OperationResultOf<TEntity>> TryAddAsync(TEntity newEntity,
+        public async Task<OperationResultOf<TEntity>> TryAddAsync(
+            TEntity newEntity,
             CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            using var connection = await GetOpenedConnectionAsync().ConfigureAwait(false);
+            int numberOfChanges = await connection.InsertAsync(newEntity).ConfigureAwait(false);
+            var success = numberOfChanges > 0;
+
+            return success ?
+                newEntity.AsSuccessfulOpRes() :
+                "".AsFailedOpResOf<TEntity>();
         }
 
-        public Task<OperationResultOf<TEntity>> TryAddOrUpdateAsync(TKey key,
+        public Task<OperationResultOf<TEntity>> TryAddOrUpdateAsync(
+            TKey key,
             Action<TEntity> updateOrInitAction,
             CancellationToken cancellationToken = default)
         {
             throw new NotImplementedException();
         }
 
-        public Task<OperationResultOf<IReadOnlyCollection<TEntity>>> TryGetMultipleAsync(
+        public async Task<OperationResultOf<PagedItemsResultOf<TEntity>>> TryGetMultipleAsync(
             Expression<Func<TEntity, bool>> filter,
             Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
             CancellationToken cancellationToken = default,
+            PagingParams pagingParams = null,
             params Expression<Func<TEntity, object>>[] toBeIncluded)
         {
-            throw new NotImplementedException();
+            using var connection = await GetOpenedConnectionAsync().ConfigureAwait(false);
+            var where = filter.ToSql();
+
+            var sqlQuery = $@"SELECT * FROM [{_tableName}]
+                            {{where}}";
+            var result = await connection.QueryAsync<TEntity>(sqlQuery, filter).ConfigureAwait(false);
+            return new PagedItemsResultOf<TEntity>(result.ToArray()).AsSuccessfulOpRes();
         }
 
-        public Task<OperationResultOf<PagedItemsResultOf<TEntity>>> TryGetMultipleAsync(Expression<Func<TEntity, bool>> filter, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, CancellationToken cancellationToken = default, PagingParams pagingParams = null, params Expression<Func<TEntity, object>>[] toBeIncluded)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<OperationResultOf<TEntity>> TryGetSingleAsync(TKey key,
+        public async Task<OperationResultOf<TEntity>> TryGetSingleAsync(
+            TKey key,
             CancellationToken cancellation = default,
             params Expression<Func<TEntity, object>>[] toBeIncluded)
         {
-            throw new NotImplementedException();
+            using var connection = await GetOpenedConnectionAsync().ConfigureAwait(false);
+            var foundEntity = await connection.GetAsync<TEntity>(key).ConfigureAwait(false);
+            return foundEntity != null ? foundEntity.AsSuccessfulOpRes() :
+                $"Failed to find {typeof(TEntity).Name} by {key}".AsFailedOpResOf<TEntity>();
         }
 
-        public Task<OperationResultOf<TEntity>> TryRemoveAsync(TKey key,
+        public async Task<OperationResultOf<TEntity>> TryRemoveAsync(
+            TKey key,
             CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            var getOpRes = await TryGetSingleAsync(key, cancellationToken).ConfigureAwait(false);
+            if (!getOpRes) return getOpRes;
+            using var connection = await GetOpenedConnectionAsync().ConfigureAwait(false);
+            var success = await connection.DeleteAsync<TEntity>(getOpRes.Value).ConfigureAwait(false);
+
+            return success ? getOpRes :
+                $"Failed to remove {typeof(TEntity).Name} by {key}".AsFailedOpResOf<TEntity>();
         }
 
-        public Task<OperationResultOf<TEntity>> TryUpdateAsync(TKey key,
+        public async Task<OperationResultOf<TEntity>> TryUpdateAsync(
+            TKey key,
             Action<TEntity> updateAction,
             CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            var getOpRes = await TryGetSingleAsync(key, cancellationToken).ConfigureAwait(false);
+            if (!getOpRes) return getOpRes;
+            using var connection = await GetOpenedConnectionAsync().ConfigureAwait(false);
+
+            var existingEntity = getOpRes.Value;
+            updateAction(existingEntity);
+
+            var success = await connection.UpdateAsync<TEntity>(existingEntity).ConfigureAwait(false);
+
+            return success ? existingEntity.AsSuccessfulOpRes() :
+                 $"Failed to update {typeof(TEntity).Name} with key:{key}".AsFailedOpResOf<TEntity>();
         }
     }
 }
---
+++ Impl/Dapper/Roman.Ambinder.Storage.Impl.Dapper/IDbConnectionProvider.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Common;
+
+namespace Roman.Ambinder.Storage.Impl.Dapper
+{
+    public interface IDbConnectionProvider
+    {
+        DbConnection Get();
+    }
+}
